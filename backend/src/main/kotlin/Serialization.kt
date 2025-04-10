c090e5d7f5e1d170981f6f247ea79f65e26bfe7e
Shreeya Maskey <shreeyamaskey.1@gmail.com>
2025-04-10 01:26:12 -0600

    creating ktor project structure

---
+++ backend/src/main/kotlin/Serialization.kt
@@ -0,0 +1,9 @@
+package server.com
+
+import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
+
+fun Application.configureSerialization() {
+}
